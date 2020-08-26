20dbb24861a39411217596f46debe9524b07678e
TheJohnLe <jle17@mail.sfsu.edu>
2020-08-26 02:58:24 -0700

    Frontend: added SingleSignOn file and imported initial dependencies

---
+++ frontend/src/components/login/SingleSignOn.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SingleSignOn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Here is the login section.</div>;
+  }
+}
+
+export default SingleSignOn;
